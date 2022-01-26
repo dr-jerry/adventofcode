146dede19f3e333d786582a8f60bc47ce2f85c7e
jeroen dijkmeijer <jeroen@stretchsketch.com>
2022-01-26 09:23:04 +0100

    day19 mapped the observations per scanner

--- src/main/scala/aoc2021/Day19.scala
+++ src/main/scala/aoc2021/Day19.scala
@@ -12,12 +12,48 @@ object Pos3 {
   }
 }
 case class Pos3 (var x: Int, var y: Int, var z: Int, scanner: Int, index: Int) {
-  def dist(that: Pos3): String = "%.4f".format(Math.sqrt((x-that.x)*(x-that.x)
-    + (y-that.y)*(y-that.y) + (z-that.z)*(z-that.z))).reverse.padTo(11, '0').reverse
+  def dist(that: Pos3): Double = Math.sqrt((x-that.x)*(x-that.x)
+    + (y-that.y)*(y-that.y) + (z-that.z)*(z-that.z))
+  def diff(that: Pos3): Pos3 = Pos3(this.x - that.x, this.y - that.y, this.z - that.z, this.scanner, this.index)
+  def pos3Equal(that: Pos3) = {
+    (this.x == that.x && this.y == that.y && this.z == that.z)
+  }
+  def rotate(ax: Char, theta: Double): Pos3 = {
+    // if theta is Int your sin/cos calculation will go sout.
+    // 2 rotations: one for direction: up,dow, north, south, east, west.
+    // one for the rotation.
+    if (theta == 0) this
+    else {
+      val (sin, cos) = (Math.sin(theta / 180 * Math.PI).toInt, Math.cos(theta / 180 * Math.PI).toInt)
+      //println(s"$theta: sin is $sin ${Math.sin(theta.toDouble / 180 * Math.PI)}, cos $cos (${Math.cos(theta.toDouble / 180 * Math.PI)})")
+      //println(s"$theta: sin is $sin, cos $cos)")
+      ax match {
+        //        Z-axis would be
+        //
+        //          |cos θ   −sin θ   0| |x|   |x cos θ − y sin θ|   |x'|
+        //        |sin θ    cos θ   0| |y| = |x sin θ + y cos θ| = |y'|
+        //        |  0       0      1| |z|   |        z        |   |z'|
+        case 'Z' => Pos3(this.x * cos - this.y * sin, this.x * sin + this.y * cos, this.z, this.scanner, this.index)
+        //        around the Y-axis would be
+        //
+        //        | cos θ    0   sin θ| |x|   | x cos θ + z sin θ|   |x'|
+        //        |   0      1       0| |y| = |         y        | = |y'|
+        //        |−sin θ    0   cos θ| |z|   |−x sin θ + z cos θ|   |z'|
+        case 'Y' => Pos3(this.x * cos + this.z * sin, this.y, -this.x * sin + this.z * cos, this.scanner, this.index)
+        //        around the X-axis would be
+        //
+        //        |1     0           0| |x|   |        x        |   |x'|
+        //        |0   cos θ    −sin θ| |y| = |y cos θ − z sin θ| = |y'|
+        //        |0   sin θ     cos θ| |z|   |y sin θ + z cos θ|   |z'|
+        case 'X' => Pos3(this.x, this.y * cos - this.z * sin, this.y * sin + this.z * cos, this.scanner, this.index)
+      }
+    }
+  }
+
 }
 
 object Day19 extends App {
-  var array = Source.fromInputStream(this.getClass.getResourceAsStream("day19.txt")).getLines().mkString("\n").split("\n\n")
+  var array = Source.fromInputStream(this.getClass.getResourceAsStream("day19b.txt")).getLines().mkString("\n").split("\n\n")
   val pos3re = "(-?\\d+),(-?\\d+),(-?\\d+)".r
   val scannere = "--- scanner (\\d+) ---".r
 
@@ -48,8 +84,29 @@ object Day19 extends App {
     val matchs =c2c(key).flatMap(tup=> List(tup._2, tup._3, tup._4, tup._5)).groupBy(identity).view.mapValues(l => l.size).toMap
     key -> matchs.keys.filter(pair => matchs(pair) > 1)}).toMap
 
+   val axis = List('X', 'Y', 'Z')
+   val angles = List(0, 90, 180, 270)
+   val rotations = for {
+     ax <- axis
+     an <- angles
+     sec <- for {
+       ax2 <- axis
+       a2 <- angles
+       if (ax2 != ax && (ax == 'X' || ax == 'Y' && ax2 == 'Z')) } yield (ax2, a2)
+   } yield ((ax, an), sec)
+
+  println(s"rotations: $rotations")
    mappedObservations.keys.foreach(key => {
-     println(s"$key -> ${mappedObservations(key).mkString("\n   ")}")
+     val theList = mappedObservations(key)
+     val normalizedList = theList.map(coor_pair => (coor_pair._1.diff(theList.head._1), coor_pair._2.diff(theList.head._2)))
+     val r = rotations.find(rot => {
+       normalizedList.forall(p => {
+         val trial = p._2.rotate(rot._1._1, rot._1._2).rotate(rot._2._1, rot._2._2)
+         //println(s"$rot: ${p._2} -> $trial ref ${p._1}")
+         trial.pos3Equal(p._1)
+       })
+     })
+     println(s"found $key => $r")
    })
 
 
